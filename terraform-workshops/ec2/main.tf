b077cbe72bb3c18ab5d42589aa76891c10af800b
Anna Lach <anna.lach.dev@gmail.com>
2021-09-14 07:07:31 +0200

    Add basic EC2

---
+++ terraform-workshops/ec2/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-central-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-091f21ecba031b39a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "TerraformWorkshops"
+  }
+}

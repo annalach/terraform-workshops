744001e4927b5131db267a3d2140f9b495cd4045
Anna Lach <anna.lach.dev@gmail.com>
2021-09-15 07:28:45 +0200

    Add Data Source for AMI id

--- terraform-workshops/ec2/main.tf
+++ terraform-workshops/ec2/main.tf
@@ -14,8 +14,24 @@ provider "aws" {
   region  = "eu-central-1"
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "web" {
-  ami           = "ami-091f21ecba031b39a"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
 
   tags = {

8f5b3ed20b83d9b21a620b41c5a7db7e9f3643a3
Anna Lach <anna.lach.dev@gmail.com>
2021-09-25 00:00:20 +0200

    Add database

---
+++ terraform-workshops/database/main.tf
@@ -0,0 +1,68 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-central-1"
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "local"
+
+  config = {
+    "path" = "../vpc/terraform.tfstate"
+  }
+}
+
+data "terraform_remote_state" "secrets" {
+  backend = "local"
+
+  config = {
+    "path" = "../secrets/terraform.tfstate"
+  }
+}
+
+data "aws_secretsmanager_secret_version" "db_secret" {
+  secret_id = data.terraform_remote_state.secrets.outputs.db_secert_arn
+}
+
+resource "aws_db_subnet_group" "rds" {
+  subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnet_ids
+
+  tags = {
+    Name = "TerraformWorkshopsDBSubnetGroup"
+  }
+}
+
+resource "aws_security_group" "rds" {
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+  description = "Security Group for RDS in Private Subnet"
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = data.terraform_remote_state.vpc.outputs.private_subnets_cidr_blocks
+  }
+}
+
+resource "aws_db_instance" "rds" {
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 5
+  engine                 = "postgres"
+  engine_version         = "13.1"
+  name                   = jsondecode(data.aws_secretsmanager_secret_version.db_secret.secret_string).name
+  username               = jsondecode(data.aws_secretsmanager_secret_version.db_secret.secret_string).username
+  password               = jsondecode(data.aws_secretsmanager_secret_version.db_secret.secret_string).password
+  db_subnet_group_name   = aws_db_subnet_group.rds.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+  skip_final_snapshot    = true
+}
